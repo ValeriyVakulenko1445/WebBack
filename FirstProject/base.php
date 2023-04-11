31226feb17219b01a84b855de9ac7295dd984d90
Valery <113440912+ValeriyVakulenko1445@users.noreply.github.com>
2023-04-11 20:11:20 +0300

    Add files via upload

---
+++ FirstProject/base.php
@@ -0,0 +1,25 @@
+$user = 'db';
+$pass = '123';
+$db = new PDO('mysql:host=localhost;dbname=test',
+$user, $pass,
+[PDO::ATTR_PERSISTENT => true,
+PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+
+try {
+$stmt = $db->prepare("INSERT INTO application SET name = ?");
+$stmt -> execute(['fio']);
+}
+catch(PDOException $e){
+print('Error : ' . $e->getMessage());
+exit();
+}
+
+$stmt = $db->prepare("INSERT INTO users (firstname, lastname, email)
+VALUES (:firstname, :lastname, :email)");
+$stmt->bindParam(':firstname', $firstname);
+$stmt->bindParam(':lastname', $lastname);
+$stmt->bindParam(':email', $email);
+$firstname = "John";
+$lastname = "Smith";
+$email = "john@test.com";
+$stmt->execute();
